0892768f12e879175f352b57c809ebf88e9ee9df
lyazj <seeson@pku.edu.cn>
2021-08-18 17:34:19 +0800

    Fix `description`.

--- Cache.py
+++ Cache.py
@@ -6,9 +6,9 @@ import json
 description = { }
 with open('name.txt', 'w') as name:
     for par in particle.particle.particle.Particle.all():
-        name.write(f'{int(par.pdgid)}:{par.name}\n')
+        # name.write(f'{int(par.pdgid)}:{par.name}\n')
         name.write(f'{int(par.pdgid)}:{par.html_name}\n')
-        # description[par.pdgid] = par.describe()
+        description[par.pdgid] = par.describe()
 
 with open('description.json', 'w') as descript:
     descript.write(json.dumps(description))
